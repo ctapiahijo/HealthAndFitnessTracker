c48ef90b9d064186cf819cf5821ea4dfb25b160f
ctapiahijo <ctapiahijo@yahoo.es>
2023-11-21 22:15:20 -0500

    FoodLog Class Completed.

---
+++ FoodLog.cs
@@ -0,0 +1,33 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+
+#region FoodLogClass
+namespace HealthAndFitnessTracker
+{
+    public class FoodLog
+    {
+        #region Properties
+        [Key]
+        public int FoodlogId { get; set; }
+        public DateTime FoodlogDate { get; set; }
+        public string MealType { get; set; } = string.Empty;
+        public string FoodItem { get; set; } = string.Empty;
+        public double FoodCalories { get; set; }
+        public int PersonId { get; set; }
+        #endregion
+
+        #region Constructors
+        public FoodLog() { }
+        public FoodLog(DateTime foodlogDate, string mealType, string foodItem, double foodCalories, int PersonId)
+        {
+
+            this.FoodlogDate = foodlogDate;
+            this.MealType = mealType;
+            this.FoodItem = foodItem;
+            this.FoodCalories = foodCalories;
+            this.PersonId = PersonId;
+        }
+        #endregion
+    }
+}
+#endregion
